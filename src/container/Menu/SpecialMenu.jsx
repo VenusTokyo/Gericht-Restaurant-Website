1f80b497695a4cef81c77b74111ca87e176a0617
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2023-07-12 00:49:28 +0530

    started special menu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import {images,data} from '../../constants'
+import { SubHeading, MenuItem } from '../../components';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialMenu flex__center section__padding' id='menu'>
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits your palatte"/>
+      <h1 className="headtext__cormorant">Today's Menu</h1>
+    </div>
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p className='app__specialMenu-menu_heading'>Wine & Beer</p>
+        <div className="app__specialMenu-menu_items">
+          
+        </div>
+      </div>
+
+    </div>
   </div>
 );
 
