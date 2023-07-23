3c5f403c0a06011dc8c8a404a8e2b7d19a00b8a8
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2023-07-23 20:06:59 +0530

    project done

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -8,7 +8,7 @@ const Chef = () => (
     <div className="app__wrapper_img app__wrapper_img-reverse" >
       <img src={images.chef} alt="chef" />
     </div>
-    <div className="app__wrapper-info">
+    <div className="app__wrapper_info">
       <SubHeading title="Chef's Word"/>
       <h1 className="headtext__cormorant">What We Believe In</h1>
       <div className="app__chef-content">
